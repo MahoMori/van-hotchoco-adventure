b567cad9028c0f86d5aca66788d72838edc5561a
MahoMori <tr27e1104@gmail.com>
2022-04-07 10:36:13 -0700

    added async to getCurrentLocation

--- src/redux/shopSlice.ts
+++ src/redux/shopSlice.ts
@@ -2,6 +2,18 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { JsonProps, LocationPropsF, ReduxState } from "../assets/tsInterface";
 import { v4 as uuid } from "uuid";
 
+interface optionsParams {
+  enableHighAccuracy: boolean;
+  timeout: number;
+  maximumAge: number;
+}
+
+const getCoordinates = (options: optionsParams) => {
+  return new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(resolve, reject, options)
+  );
+};
+
 const initialState: ReduxState = {
   shops: [],
 };
@@ -50,8 +62,8 @@ export const shopSlice = createSlice({
 
       // ---- close to current location ----
       const shopLocation: LocationPropsF = {
-        lat: 49.217789,
-        lng: -123.060396,
+        lat: 49.2177522,
+        lng: -123.0604064,
       };
 
       let currentLocation: LocationPropsF = { lat: 0, lng: 0 };
@@ -61,15 +73,33 @@ export const shopSlice = createSlice({
       //   lng: -123.0604381,
       // };
 
+      let result: boolean = false;
+
       // ----- helper functions -----
-      const getCurrentLocation = (): void => {
-        navigator.geolocation.getCurrentPosition((position) => {
+      const getCurrentLocation = async () => {
+        const options = {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0,
+        };
+
+        try {
+          const position: any = await getCoordinates(options);
           currentLocation.lat = position.coords.latitude;
           currentLocation.lng = position.coords.longitude;
 
-          console.log("inside getCurrentPosition", currentLocation);
-        });
+          result = arePointsNear(currentLocation, shopLocation, 0.1);
+          console.log("I'll get printed first", currentLocation, result);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            return {
+              message: `Unable to get current location (${err.message})`,
+            };
+          }
+        }
+
         console.log("outside getCurrentPosition", currentLocation);
+        changeStateOrAlert();
       };
 
       const arePointsNear = (
@@ -83,39 +113,43 @@ export const shopSlice = createSlice({
         const dy = Math.abs(hcShopLocation.lat - userLocation.lat) * ky;
         return Math.sqrt(dx * dx + dy * dy) <= km;
       };
+
+      const changeStateOrAlert = () => {
+        if (result) {
+          const newEachStoreInfo = payloadShop.eachStoreInfo.map(
+            (eachStore) => {
+              let newEachStore = { ...eachStore };
+
+              if (newEachStore.eachStoreId === payloadId) {
+                if (!newEachStore.beenTo) {
+                  newEachStore.beenTo = true;
+                } else {
+                  alert(
+                    "You've already been to this place! How was their hot chocolate?😋"
+                  );
+                }
+              }
+
+              return newEachStore;
+            }
+          );
+
+          payloadShop.eachStoreInfo = newEachStoreInfo;
+
+          state.shops = state.shops.map((shop) => {
+            return shop.shopName === payloadShop.shopName
+              ? (shop = payloadShop)
+              : shop;
+          });
+        } else {
+          alert(
+            "Come closer to the shop and treat yourself with a warm hot chocolate😉"
+          );
+        }
+      };
       // ----- -----
 
       getCurrentLocation();
-
-      if (arePointsNear(currentLocation, shopLocation, 0.1)) {
-        const newEachStoreInfo = payloadShop.eachStoreInfo.map((eachStore) => {
-          let newEachStore = { ...eachStore };
-
-          if (newEachStore.eachStoreId === payloadId) {
-            if (!newEachStore.beenTo) {
-              newEachStore.beenTo = true;
-            } else {
-              alert(
-                "You've already been to this place! How was their hot chocolate?😋"
-              );
-            }
-          }
-
-          return newEachStore;
-        });
-
-        payloadShop.eachStoreInfo = newEachStoreInfo;
-
-        state.shops = state.shops.map((shop) => {
-          return shop.shopName === payloadShop.shopName
-            ? (shop = payloadShop)
-            : shop;
-        });
-      } else {
-        alert(
-          "Come closer to the shop and treat yourself with a warm hot chocolate😉"
-        );
-      }
     },
   },
 });
